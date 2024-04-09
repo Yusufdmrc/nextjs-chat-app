1285f5998b20159044c46dd3366af9e9b1f0fefe
yusuf <yusuffdemirci10@gmail.com>
2024-04-09 19:05:55 +0300

    Input component created

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,6 +1,10 @@
 "use client";
 
 import { useCallback, useState } from "react";
+import { act } from "react-dom/test-utils";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+import Input from "../../components/inputs/Input";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -16,7 +20,45 @@ const AuthForm = () => {
     }
   }, [variant]);
 
-  return <div>Auth Form</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    if (variant === "REGISTER") {
+      //Axios Register
+    }
+
+    if (variant === "LOGIN") {
+      //NextAuth SignIn
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+
+    //NextAuth Social Sign In
+  };
+
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          <Input label="Email" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AuthForm;
