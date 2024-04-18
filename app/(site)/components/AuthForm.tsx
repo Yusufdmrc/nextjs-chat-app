a794c4c1ea157d6c5c2a458270771bf87f5191c1
yusuf <yusuffdemirci10@gmail.com>
2024-04-18 19:01:37 +0300

    Google Client added

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -8,6 +8,8 @@ import Input from "../../components/inputs/Input";
 import Button from "@/app/components/Button";
 import AuthSocialButton from "./AuthSocialButton";
 import axios from "axios";
+import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -39,18 +41,42 @@ const AuthForm = () => {
     setIsLoading(true);
 
     if (variant === "REGISTER") {
-      axios.post("/api/register", data);
+      axios
+        .post("/api/register", data)
+        .catch(() => toast.error("Bir şeyler yanlış gitti!"))
+        .finally(() => setIsLoading(false));
     }
 
     if (variant === "LOGIN") {
-      //NextAuth SignIn
+      signIn("credentials", {
+        ...data,
+        redirect: false,
+      })
+        .then((callback) => {
+          if (callback?.error) {
+            toast.error("Geçersiz kimlik");
+          }
+          if (callback?.ok && !callback?.error) {
+            toast.success("Giriş yapıldı!");
+          }
+        })
+        .finally(() => setIsLoading(false));
     }
   };
 
   const socialAction = (action: string) => {
     setIsLoading(true);
 
-    //NextAuth Social Sign In
+    signIn(action, { redirect: false })
+      .then((callback) => {
+        if (callback?.error) {
+          toast.error("Geçersiz kimlik");
+        }
+        if (callback?.ok && !callback?.error) {
+          toast.success("Giriş yapıldı");
+        }
+      })
+      .finally(() => setIsLoading(false));
   };
 
   return (
